dfcfbd00f1aa8876a8accb81c121306b5a3e3704
Ivar Stangeby <istangeby@gmail.com>
2018-09-04 15:46:45 +0200

    Added extra asserts in meshline tests.

--- tests/test_meshline.py
+++ tests/test_meshline.py
@@ -21,27 +21,35 @@ def test_meshline_init():
 def test_meshline_splits_element():
     start = 0
     stop = 1
-    axis = 0
     constant_value = 0.5
-    m = Meshline(start, stop, constant_value, axis)
 
+    m1 = Meshline(start, stop, constant_value, 0)
+    m2 = Meshline(start, stop, constant_value, 1)
     e1 = Element(0, 0, 1, 1)
     e2 = Element(0, 0, 0.4, 1)
     e3 = Element(0, 0, 0.5, 1)
-    assert m.splits_element(e1)
-    assert not m.splits_element(e2)
-    assert not m.splits_element(e3)
+
+    assert m1.splits_element(e1)
+    assert not m1.splits_element(e2)
+    assert not m1.splits_element(e3)
+
+    assert m2.splits_element(e1)
+    assert m2.splits_element(e2)
+    assert m2.splits_element(e3)
 
 
 def test_meshline_splits_basis():
     start = 0
     stop = 1
-    axis = 0
     constant_value = 0.5
-    m = Meshline(start, stop, constant_value, axis)
+    m1 = Meshline(start, stop, constant_value, 0)
+    m2 = Meshline(start, stop, constant_value, 1)
 
     b1 = BSpline(2, 2, [0, 1, 2, 3], [0, 1, 2, 3])
     b2 = BSpline(2, 2, [0, 1, 2, 3], [0, 0.25, 0.5, 0.75, 1])
 
-    assert not m.splits_basis(b1)
-    assert m.splits_basis(b2)
+    assert not m1.splits_basis(b1)
+    assert m1.splits_basis(b2)
+
+    assert not m2.splits_basis(b1)
+    assert not m2.splits_basis(b2)
