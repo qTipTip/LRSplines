ccee105f13d90a43dbf1b8d41f9a30c2bf129f02
Ivar Stangeby <istangeby@gmail.com>
2018-09-05 13:34:38 +0200

    Added helper method for initializing a tensor product LRSpline.

--- src/lr_spline.py
+++ src/lr_spline.py
@@ -1,7 +1,40 @@
+import typing
 from typing import List
 
+import numpy as np
+
+from meshline import Meshline
 from src.b_spline import BSpline
 from src.element import Element
+
+Vector = typing.Union[typing.List['float'], np.ndarray]
+
+
+def init_tensor_product_LR_spline(d1: int, d2: int, ku: Vector, kv: Vector) -> LRSpline:
+    """
+    Initializes an LR spline at the tensor product level of bidegree (d1, d2).
+    :param d1: first component degree
+    :param d2: second component degree
+    :param ku: knots in u_direction
+    :param kv: knots in v_direction
+    :return: corresponding LR_spline
+    """
+    elements = []
+    basis = []
+    for i in range(len(ku) - 1):
+        for j in range(len(kv) - 1):
+            elements.append(Element(ku[i], kv[j], ku[i + 1], kv[j + 1]))
+
+    for i in range(len(ku) - d1 - 1):
+        for j in range(len(kv) - d2 - 1):
+            basis.append(BSpline(d1, d2, ku[i: i + d1 + 2], kv[j: j + d2 + 2]))
+
+    for b in basis:
+        for e in elements:
+            if b.add_to_support_if_intersects(e):
+                e.add_supported_b_spline(b)
+
+    return LRSpline(elements, basis)
 
 
 class LRSpline(object):
@@ -34,3 +67,12 @@ class LRSpline(object):
         :param e: element to refine
         """
         raise NotImplementedError('LRSpline.{} is not implemented yet'.format(self.refine_by_element_minimal.__name__))
+
+    def get_minimal_span_meshline(self, e: Element, axis=0) -> Meshline:
+        """
+        Finds the shortest possible meshline in direction prescribed by axis that splits at least one supported B-spline on the element.
+        :param e: element to refine by
+        :param axis: direction to look for split, 0 vertical, 1 horizontal
+        :return: minimal span meshline
+        """
+        raise NotImplementedError('LRSpline.{} is not implemented yet'.format(self.get_minimal_span_meshline.__name__))
